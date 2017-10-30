1ec6ff241772d752a61c6fbb945b898c18120556
MFanjul <31902170+MFanjul@users.noreply.github.com>
2017-10-30 16:55:22 -0300

    Update Tarjeta.php

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -11,4 +11,29 @@ class Tarjeta {
         
         return 0;
     }
+    public function recargar($monto){
+        if($monto==500){
+        $this->saldo = $this->saldo + 640;
+        }
+        else{
+        if($monto==272){
+        $this->saldo = $this->saldo + 320;
+            }
+            else{
+            if($monto==320){
+            $this->saldo = $this->saldo + 388;
+                }
+                else{
+                $this->saldo = $this->saldo + $monto;
+                }
+            }
+        }
+        
+    }
+    public function viajesRealizados(){
+    }
+    public function pagar(Transporte $transporte, $fecha_y_hora){
+    }
+
+
 }
