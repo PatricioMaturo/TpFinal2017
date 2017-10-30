c3d869a7549006a5f0bd15d441eff1fb14aa1c20
PatricioMaturo <32202550+PatricioMaturo@users.noreply.github.com>
2017-10-29 22:16:08 -0300

    Update Tarjeta.php

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -1,11 +1,65 @@
 <?php
-
 namespace TpFinal;
-
 class Tarjeta {
 
+	public $saldo=0;	
+
+	public function CargarSaldo($monto){
+
+	if (this->$monto=332){
+
+	this->monto=388;
+
+} 
+	if (this->$monto=624){
+
+	this->monto=776;
+
+} 
+	$this->saldo=$this->saldo + $monto;
+	
+
+}
     
-    public function saldo() {
-        return 0;
+    public function Saldo() {
+	
+	echo saldo;
+	
+	return 0;
     }
+
+
+	public function viaje(){
+	
+	saldo=saldo - 8.5;
+
+
+}
+
+
+	
+}
+
+class Colectivo{
+
+		public $linea;
+
+		public function transbordo(){
+
+	
+	
+	
+}
+		
+
+
+}
+
+class Bicicleta{
+
+		
+
+
+
+
 }
