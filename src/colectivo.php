10e72ef1eb003ad989e26fb0cd05f5c58c3709ce
PatricioMaturo <32202550+PatricioMaturo@users.noreply.github.com>
2017-10-30 17:13:45 -0300

    Update and rename colectivo to colectivo.php
    Creada clase colectivo,que contiene la linea del mismo

---
+++ src/colectivo.php
@@ -0,0 +1,19 @@
+
+namespace TpFinal;
+
+Class Colectivo {
+
+public $linea;
+
+public function __construct($linea){
+
+$this->linea=$linea;
+}
+
+
+public function mostrarlinea{
+
+echo $this->linea;
+}
+
+}
