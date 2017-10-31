9645e49b39756128748e20b63c9153c76ea741c6
MFanjul <31902170+MFanjul@users.noreply.github.com>
2017-10-31 00:17:40 -0300

    Update Tarjeta.php

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -7,11 +7,16 @@ class Tarjeta {
     protected $dni;
     protected $tipo;
     protected $vplus;
-    protected $fechaviaje;
+    protected $horaviaje;
+    protected $horaant;
+    protected $diaviaje;
+    protected $diaant;
 
     public function __construct($dni, $tipos){
     $this->saldo = 0;
     $this->dni = $dni;
+    $this->horaant = 10.91;
+    $this->diaant = 32.13;
     $this->tipo = $tipos; //LOS TIPOS SON NORMAL, MEDIO BOLETO O BICI, ES PARA HACER LAS FUNCIONES DESPUES
     $this->vplus = 0; //ESTO ES PARA VER CUANTOS VIAJES PLUS LE QUEDAN, SE VA MODIFICANDO CON LAS FUNCIONES, Y VUELVE A 0 CUANDO SE LE CARGA MAS SALDO DE LO QUE SE DEBE
 }
@@ -53,23 +58,33 @@ class Tarjeta {
     }
     public function viajesRealizados(){
     }
-    public function pagar(Transporte $transporte, $fecha_y_hora){ 
+    public function pagar($transporte , $hora , $dia){ 
         
         //ACA VA LA FUNCION PARA DETERMINAR CUAL FUNCION DE LAS DE ABAJO ES LA QUE SUCEDE
         //LO HACEMOS VIENDO EL TIPO DE TARJETA QUE TIENE LA PERSONA (MEDIO, NORMAL, BICI)
-		
-		if( $this->tipo == "Normal" ){
+		if(is_a ($transporte , 'Colectivo') ){
+		        $this->horaviaje = $hora;
+			$this->diaviaje = $dia;
+			if($this->diaviaje == $this->diaant){
+			if($this->horaviaje-$this->horaant<0.30){
+			$this->trasbordo();
+			}
+			}
+			else{
+	if( $this->tipo == "Normal" ){
                 $this->normal();
         	}
         if( $this->tipo == "MedioBoleto" ){
                 $this->medio(); 
        }   
-        if(is_a ($transporte , 'Colectivo') ){
-		        $this->fechaviaje = new DateTime ();
-        }
-        if((is_a ($transporte , 'Bicicleta') ){
+		}
+	   
+        if(is_a ($transporte , 'Bicicleta') ){
 		        $this->viajeBici();
     	}
+	    $this->horaant = $this->horaviaje;
+	    $this->diaant = $this->diaviaje;
+    }
     public function normal(){
     if($this->saldo < 9.70){
         $this->plus();
@@ -95,7 +110,22 @@ class Tarjeta {
         }
     }
     public function trasbordo(){
-        
+        if($this->tipo == "MedioBoleto"){
+	if($this->saldo < 1.60){
+	$this->plus();
+	 }
+		else{
+		$this->saldo = $this->saldo - 1.60;
+		}
+	}
+	    else{
+	    if($this->saldo < 3.20){
+	$this->plus();
+	 }
+		else{
+		$this->saldo = $this->saldo - 3.20;
+		}
+	    }
         
     }
     public function bici(){
