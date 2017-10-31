6bb237443af15a181793cbf72dc861b2af26a5b9
MFanjul <31902170+MFanjul@users.noreply.github.com>
2017-10-31 14:54:23 -0300

    Update TarjetaTest.php

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -10,19 +10,21 @@ class TestTarjeta extends TestCase {
     	public function testSaldoCero() {
         	$tarjeta = new Tarjeta(41559067,"Normal");
         	$this->assertEquals( $tarjeta->saldo(), 0 );
+	}
    
     	public function testcincuenta(){
         	$tarjeta50 = new Tarjeta(41559067,"Normal");
         	$tarjeta50->recargar(50);
         	$this->assertEquals($tarjeta50->saldo(),50);
-    }
-    	public function test332(){
+    	}
+    	
+	public function test332(){
     
         	$tarjeta332 = new Tarjeta(41559067,"Normal");
         	$tarjeta332->recargar(332);
 		$this->assertEquals($tarjeta332->saldo(),388);
     
-    }
+    	}
     
     	public function testviaje() {
 	 	$144roja = new Colectivo ( "144 roja" );
