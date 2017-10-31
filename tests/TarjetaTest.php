3b42292e6dced86c96fa302e37e79beadd0a4728
MFanjul <31902170+MFanjul@users.noreply.github.com>
2017-10-31 16:11:49 -0300

    Update TarjetaTest.php

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -34,6 +34,7 @@ class TestTarjeta extends TestCase {
     
     
 	public function testviajex2() {
+		$roja144 = new Colectivo ( "144 roja" );
 		$tarjeta50 = new Tarjeta(23456789, "Normal");
 	    	$tarjeta50->pagar("Colectivo", 13.45, 14.10, $roja144);
 	    	$tarjeta50->pagar("Colectivo", 13.45, 14.10, $roja144);
