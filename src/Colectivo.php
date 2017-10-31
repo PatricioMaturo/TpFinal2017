0dd51739d1f14036c8eb0bb09455a2ca582a71ba
MFanjul <31902170+MFanjul@users.noreply.github.com>
2017-10-31 17:35:38 -0300

    Update Colectivo.php

--- src/Colectivo.php
+++ src/Colectivo.php
@@ -3,18 +3,11 @@
 namespace TpFinal;
 
 Class Colectivo {
-
 public $linea;
-
 public function __construct($linea) {
-
 $this->linea=$linea;
 }
-
-
 public function mostrarlinea() {
-
 return $this->linea;
 }
-
 }
