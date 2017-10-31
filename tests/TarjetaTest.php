2119c3203039c5938548f7db5e1242625597f4de
PatricioMaturo <32202550+PatricioMaturo@users.noreply.github.com>
2017-10-31 11:49:46 -0300

    Update TarjetaTest.php
    test bici

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -20,30 +20,41 @@ class TestTarjeta extends TestCase {
     
         $tarjeta332 = new Tarjeta(41559067,"Normal");
         $tarjeta332->recargar(332);
-        $this->assertEquals($tarjeta332->saldo(),388);
+	$this->assertEquals($tarjeta332->saldo(),388);
     
     }
     
     public function testviaje() {
-	    $144roja = new Colectivo ( "144 roja" );
-	    $tarjeta332->pagar($144roja, 13:45, 6);//Dia?
-	    $this->assertEquals( $tarjeta332->saldo(); (388-9.70) );
+	 $144roja = new Colectivo ( "144 roja" );
+	 $tarjeta332->pagar($144roja, 13:45, 6);//Dia?
+	 $this->assertEquals( $tarjeta332->saldo(); (388-9.70) );
     
     }
     
     
-	 public function testviajex2() {
+	public function testviajex2() {
 	    $tarjeta50->pagar($144roja, 13:45, 6);
 	    $this->assertEquals( $tarjeta50->saldo(); (50-9.70-9.70) );
     }
     
     
-    public function testViajeTransbordo() {
-        $Q = new Colectivo ( "Q" );
+    	public function testViajeTransbordo() {
+        	$Q = new Colectivo ( "Q" );
 		$tarjeta->recargar(50);
-	    $tarjeta->pagar($144roja);
+	    	$tarjeta->pagar($144roja);
 		$tarjeta->pagar($Q);
-	    $this->assertEquals( $tarjeta->saldo(); ((50-9.70)-3.20) );
+	    	$this->assertEquals( $tarjeta->saldo(); ((50-9.70)-3.20) );
         
     }
         
+	public function testbici(){
+		$tar2 = new Tarjeta(34567890, "Normal");
+		$bici = new Bicicleta(2345);
+		$tar2->recargar(332);
+		$tar2->bici();
+		$this->assertEquals( $tarjeta->saldo(); 388-14.55 );
+	
+	}
+	    
+	    
+	    
