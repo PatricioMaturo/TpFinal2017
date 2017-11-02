c7d7f88eb8583e4e8b67b4825204145590e70ddb
PatricioMaturo <32202550+PatricioMaturo@users.noreply.github.com>
2017-11-02 19:25:31 -0300

    Update Bicicletatest.php

--- tests/Bicicletatest.php
+++ tests/Bicicletatest.php
@@ -6,7 +6,7 @@ namespace TpFinal;
 use PHPUnit\Framework\TestCase;
 
 class TestBicicleta extends TestCase {
-    public function idtest(){
+    public function testid(){
         $bici= new Bicicleta(5478);
         $this->assertEquals($bici->mostrarid(),5478);
     }
